a5c49b7211c1cd9bafba3ba2e1363dd163978efc
Cary Htan <112520781+caryhtan@users.noreply.github.com>
2023-09-10 11:33:49 +0800

    Update settings.py
    WhiteNoise

--- petsis/settings.py
+++ petsis/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #For Static Files
 ]
 
 ROOT_URLCONF = 'petsis.urls'
